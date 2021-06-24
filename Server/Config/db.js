618d12894bbc05d2d1d9e77068330c8c6e4a471b
TesminePoulose <tpoulose@my.centennialcollege.ca>
2021-06-24 20:13:40 +0530

    initial commit

---
+++ Server/Config/db.js
@@ -0,0 +1,10 @@
+//tesmine poulose, 301151876,24.06.2021
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost/books";
+exports.RemoteURI = "your remote host information goes here";
+exports.HostName = "LocalHost";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
